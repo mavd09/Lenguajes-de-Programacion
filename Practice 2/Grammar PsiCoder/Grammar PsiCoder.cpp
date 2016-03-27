78fec7034d0e75fbb12727d62c272502ce9812db
Osman Jimenez <odjimenezg@unal.edu.co>
2016-03-27 18:31:46 -0500

    grammar fixed

--- Practice 2/Grammar PsiCoder/Grammar PsiCoder.cpp
+++ Practice 2/Grammar PsiCoder/Grammar PsiCoder.cpp
@@ -1,9 +1,9 @@
 /// parametros funcion
 PARAMETERS -> DATATYPE id END_VAR
-END_VAR  -> tk_coma id END_VAR
+END_VAR  -> tk_coma DATATYPE id END_VAR
 END_VAR  -> epsilon
 ///declaracion de funciones
-FUNCTION_DECLARATION -> funcion CONSTANT id tk_par_izq PARAMETERS tk_par_der hacer BLOCK_INSTRUCTIONS retornar EXPRESSION1 tk_pyc fin_funcion
+FUNCTION_DECLARATION -> funcion DATATYPE id tk_par_izq PARAMETERS tk_par_der hacer BLOCK_INSTRUCTIONS retornar EXPRESSION1 tk_pyc fin_funcion
 /// funcion principal
 MAIN_FUNCTION -> FUNCTIONS_ADT funcion_principal BLOCK_INSTRUCTIONS fin_principal FUNCTIONS_ADT
 FUNCTIONS_ADT -> FUNCTION_DECLARATION FUNCTIONS_ADT
@@ -12,7 +12,7 @@ FUNCTIONS_ADT -> epsilon
 ///imprimir
 PRINT -> imprimir tk_par_izq EXPRESSION1 MORE tk_par_der tk_pyc
 MORE  -> tk_coma EXPRESSION1 MORE
-MORE  -> epsilion
+MORE  -> epsilon
 /// leer
 READ -> leer tk_par_izq ID tk_par_der tk_pyc
 /// asignaciones
@@ -20,13 +20,13 @@ VALUE_ASSIGNMENT -> ID tk_asig EXPRESSION1 tk_pyc
 /// id
 ID  -> id ID*
 ID* -> tk_punto id ID*
-ID* -> epsilion
+ID* -> epsilon
 /// llamado de funciones
 CALL_FUNCTION -> ID tk_par_izq PARAMETERS tk_par_der tk_pyc
 PARAMETERS    -> id PARAMETERS*
-PARAMETERS    -> epsilion
+PARAMETERS    -> epsilon
 PARAMETERS*   -> tk_coma id PARAMETERS*
-PARAMETERS*   -> epsilion
+PARAMETERS*   -> epsilon
 ///Condicionales ---> Pendiente
 IF_CONDITIONAL  -> si tk_par_izq EXPRESSION1 tk_par_der entonces BLOCK_INSTRUCTIONS IF_CONDITIONAL*
 IF_CONDITIONAL* -> fin_si
@@ -43,7 +43,7 @@ LOOP_FOR  -> para tk_par_izq LOOP_FOR1 tk_pyc LOOP_FOR2 tk_pyc LOOP_FOR3 tk_par_
 LOOP_FOR1 -> DATATYPE id tk_asig EXPRESSION1
 LOOP_FOR1 -> id tk_asig EXPRESSION1
 LOOP_FOR2 -> EXPRESSION1
-LOOP_FOR2 -> epsilion
+LOOP_FOR2 -> epsilon
 LOOP_FOR3 -> LOOP_FOR2
 /// Bloque
 BLOCK_INSTRUCTIONS -> PRINT BLOCK_INSTRUCTIONS
@@ -58,13 +58,13 @@ BLOCK_INSTRUCTIONS -> VARIABLE_DECLARATION BLOCK_INSTRUCTIONS
 /// Declaracion de TDA
 ADT  -> estructura id VARIABLE_DECLARATION ADT* fin_estructura
 ADT* -> VARIABLE_DECLARATION ADT*
-ADT* -> epsilion
+ADT* -> epsilon
 /// creación variables
 VARIABLE_DECLARATION  -> DATATYPE VAR END_VAR
 END_VAR  -> tk_coma VAR END_VAR
 END_VAR -> tk_pyc
 VAR    -> id ASSIGN_VALUE
-ASSIGN_VALUE -> epsilion
+ASSIGN_VALUE -> epsilon
 ASSIGN_VALUE -> tk_asig EXPRESSION1
 DATATYPE  -> entero
 DATATYPE  -> real
@@ -75,38 +75,41 @@ DATATYPE  -> id
 EXPRESSION1  -> TERM EXPRESSION1*
 EXPRESSION1* -> tk_y TERM EXPRESSION1*
 EXPRESSION1* -> tk_O TERM EXPRESSION1*
-EXPRESSION1* -> epsilion
+EXPRESSION1* -> epsilon
 EXPRESSION1  -> EXPRESSION2
 EXPRESSION2  -> TERM EXPRESSION2* tk_igual TERM
 EXPRESSION2* -> tk_igual TERM EXPRESSION2*
 EXPRESSION2* -> tk_dif TERM EXPRESSION2*
-EXPRESSION2* -> epsilion
+EXPRESSION2* -> epsilon
 EXPRESSION2  -> EXPRESSION3
 EXPRESSION3  -> TERM EXPRESSION3*
 EXPRESSION3* -> tk_menor TERM EXPRESSION3*
 EXPRESSION3* -> tk_mayor TERM EXPRESSION3*
 EXPRESSION3* -> tk_menor_igual TERM EXPRESSION3*
 EXPRESSION3* -> tk_mayor_igual TERM EXPRESSION3*
-EXPRESSION3* -> epsilion
+EXPRESSION3* -> epsilon
 EXPRESSION3  -> EXPRESSION4
 EXPRESSION4  -> TERM EXPRESSION4*
 EXPRESSION4* -> tk_mas TERM EXPRESSION4*
 EXPRESSION4* -> tk_menos TERM EXPRESSION4*
-EXPRESSION4* -> epsilion
+EXPRESSION4* -> epsilon
 EXPRESSION4  -> EXPRESSION5
 EXPRESSION5  -> TERM EXPRESSION5*
 EXPRESSION5* -> tk_mult TERM EXPRESSION5*
 EXPRESSION5* -> tk_mod TERM EXPRESSION5*
 EXPRESSION5* -> tk_div TERM EXPRESSION5*
-EXPRESSION5* -> epsilion
+EXPRESSION5* -> epsilon
 TERM           -> tk_par_izq EXPRESSION1 tk_par_der
 TERM           -> CONSTANT
 TERM           -> ID
-TERM           -> True
-TERM           -> False
+TERM           -> verdadero
+TERM           -> false
 TERM           -> tk_menos TERM
 TERM           -> tk_mas TERM
 TERM           -> tk_neg TERM
 ///constantes
-CONSTANT -> tk_entero | tk_real | tk_caracter | tk_cadena
+CONSTANT -> tk_entero
+CONSTANT -> tk_real
+CONSTANT -> tk_caracter
+CONSTANT -> tk_cadena
 /**/
