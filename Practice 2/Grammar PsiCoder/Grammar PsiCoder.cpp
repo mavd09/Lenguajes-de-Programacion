876144f60f32b048b47a53c4a125b09f7e9e5bcc
Manuel Vergara <mavd09@hotmail.com>
2016-03-27 18:29:12 -0500

    Revert "epsilon fixed"
    This reverts commit 6553d5df13c27f7c588855529ed0be26a8cb269b.

--- Practice 2/Grammar PsiCoder/Grammar PsiCoder.cpp
+++ Practice 2/Grammar PsiCoder/Grammar PsiCoder.cpp
@@ -12,7 +12,7 @@ FUNCTIONS_ADT -> epsilon
 ///imprimir
 PRINT -> imprimir tk_par_izq EXPRESSION1 MORE tk_par_der tk_pyc
 MORE  -> tk_coma EXPRESSION1 MORE
-MORE  -> epsilon
+MORE  -> epsilion
 /// leer
 READ -> leer tk_par_izq ID tk_par_der tk_pyc
 /// asignaciones
@@ -20,13 +20,13 @@ VALUE_ASSIGNMENT -> ID tk_asig EXPRESSION1 tk_pyc
 /// id
 ID  -> id ID*
 ID* -> tk_punto id ID*
-ID* -> epsilon
+ID* -> epsilion
 /// llamado de funciones
 CALL_FUNCTION -> ID tk_par_izq PARAMETERS tk_par_der tk_pyc
 PARAMETERS    -> id PARAMETERS*
-PARAMETERS    -> epsilon
+PARAMETERS    -> epsilion
 PARAMETERS*   -> tk_coma id PARAMETERS*
-PARAMETERS*   -> epsilon
+PARAMETERS*   -> epsilion
 ///Condicionales ---> Pendiente
 IF_CONDITIONAL  -> si tk_par_izq EXPRESSION1 tk_par_der entonces BLOCK_INSTRUCTIONS IF_CONDITIONAL*
 IF_CONDITIONAL* -> fin_si
@@ -43,7 +43,7 @@ LOOP_FOR  -> para tk_par_izq LOOP_FOR1 tk_pyc LOOP_FOR2 tk_pyc LOOP_FOR3 tk_par_
 LOOP_FOR1 -> DATATYPE id tk_asig EXPRESSION1
 LOOP_FOR1 -> id tk_asig EXPRESSION1
 LOOP_FOR2 -> EXPRESSION1
-LOOP_FOR2 -> epsilon
+LOOP_FOR2 -> epsilion
 LOOP_FOR3 -> LOOP_FOR2
 /// Bloque
 BLOCK_INSTRUCTIONS -> PRINT BLOCK_INSTRUCTIONS
@@ -58,13 +58,13 @@ BLOCK_INSTRUCTIONS -> VARIABLE_DECLARATION BLOCK_INSTRUCTIONS
 /// Declaracion de TDA
 ADT  -> estructura id VARIABLE_DECLARATION ADT* fin_estructura
 ADT* -> VARIABLE_DECLARATION ADT*
-ADT* -> epsilon
+ADT* -> epsilion
 /// creación variables
 VARIABLE_DECLARATION  -> DATATYPE VAR END_VAR
 END_VAR  -> tk_coma VAR END_VAR
 END_VAR -> tk_pyc
 VAR    -> id ASSIGN_VALUE
-ASSIGN_VALUE -> epsilon
+ASSIGN_VALUE -> epsilion
 ASSIGN_VALUE -> tk_asig EXPRESSION1
 DATATYPE  -> entero
 DATATYPE  -> real
@@ -75,24 +75,24 @@ DATATYPE  -> id
 EXPRESSION1  -> TERM EXPRESSION1*
 EXPRESSION1* -> tk_y TERM EXPRESSION1*
 EXPRESSION1* -> tk_O TERM EXPRESSION1*
-EXPRESSION1* -> epsilon
+EXPRESSION1* -> epsilion
 EXPRESSION1  -> EXPRESSION2
 EXPRESSION2  -> TERM EXPRESSION2* tk_igual TERM
 EXPRESSION2* -> tk_igual TERM EXPRESSION2*
 EXPRESSION2* -> tk_dif TERM EXPRESSION2*
-EXPRESSION2* -> epsilon
+EXPRESSION2* -> epsilion
 EXPRESSION2  -> EXPRESSION3
 EXPRESSION3  -> TERM EXPRESSION3*
 EXPRESSION3* -> tk_menor TERM EXPRESSION3*
 EXPRESSION3* -> tk_mayor TERM EXPRESSION3*
 EXPRESSION3* -> tk_menor_igual TERM EXPRESSION3*
 EXPRESSION3* -> tk_mayor_igual TERM EXPRESSION3*
-EXPRESSION3* -> epsilon
+EXPRESSION3* -> epsilion
 EXPRESSION3  -> EXPRESSION4
 EXPRESSION4  -> TERM EXPRESSION4*
 EXPRESSION4* -> tk_mas TERM EXPRESSION4*
 EXPRESSION4* -> tk_menos TERM EXPRESSION4*
-EXPRESSION4* -> epsilon
+EXPRESSION4* -> epsilion
 EXPRESSION4  -> EXPRESSION5
 EXPRESSION5  -> TERM EXPRESSION5*
 EXPRESSION5* -> tk_mult TERM EXPRESSION5*
@@ -102,14 +102,11 @@ EXPRESSION5* -> epsilion
 TERM           -> tk_par_izq EXPRESSION1 tk_par_der
 TERM           -> CONSTANT
 TERM           -> ID
-TERM           -> verdadero
-TERM           -> falso
+TERM           -> True
+TERM           -> False
 TERM           -> tk_menos TERM
 TERM           -> tk_mas TERM
 TERM           -> tk_neg TERM
 ///constantes
-CONSTANT -> tk_entero
-CONSTANT -> tk_real
-CONSTANT -> tk_caracter
-CONSTANT -> tk_cadena
+CONSTANT -> tk_entero | tk_real | tk_caracter | tk_cadena
 /**/
